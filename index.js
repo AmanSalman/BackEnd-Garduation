abee24f925b57fc0d07071064a8bf100d93c3b87
AmanSalman <rapamansalman@gmail.com>
2024-04-25 21:12:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import 'dotenv/config'
+import { connectDB } from './DB/connection.js';
+import { Appinit } from './src/Appinit.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+ Appinit(app,express);
+
+ connectDB().then(()=>{
+    app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`);
+})
+ }).catch(err =>{
+    console.log("fail while connecting to server"+ err)
+ })
+
+
+
+
---
+++ src/Appinit.js
@@ -0,0 +1,28 @@
+
+import authRouter from './modules/auth/auth.router.js';
+import bookRouter from './modules/book/book.router.js';
+import cartRouter from './modules/cart/cart.router.js';
+import categoryRouter from './modules/category/category.router.js';
+import orderRouter from './modules/order/order.router.js';
+import reviewRouter from './modules/review/review.router.js';
+import userRouter from './modules/user/user.router.js';
+
+export const Appinit = ( app,express )=>{
+    app.use(express.json());
+
+    app.get('/', (req,res)=>{
+        return res.status(200).json({message:"Welcome"})
+     })
+
+    app.use('/auth', authRouter);
+    app.use('/user', userRouter)
+    app.use('/book', bookRouter);
+    app.use('/cart', cartRouter);
+    app.use('/category', categoryRouter);
+    app.use('/order', orderRouter);
+    app.use('/review', reviewRouter);
+
+    app.get('*', (req,res)=>{
+        return res.status(404).json({message:"page not found"})
+     })
+}
