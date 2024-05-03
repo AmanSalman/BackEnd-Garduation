770a2fa06908ffabeb4ee9c1cc62e689dfb818e8
AmanSalman <rapamansalman@gmail.com>
2024-05-04 00:35:54 +0300

    changes on cors

--- src/Appinit.js
+++ src/Appinit.js
@@ -6,10 +6,10 @@ import categoryRouter from './modules/category/category.router.js';
 import orderRouter from './modules/order/order.router.js';
 import reviewRouter from './modules/review/review.router.js';
 import userRouter from './modules/user/user.router.js';
-
+import cors from 'cors'
 export const Appinit = ( app,express )=>{
     app.use(express.json());
-
+    app.use(cors()); 
     app.get('/', (req,res)=>{
         return res.status(200).json({message:"Welcome"})
      })
