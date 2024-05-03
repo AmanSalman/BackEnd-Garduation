848421fdbb4ca7cdc2e9265883188a9fe9a4f769
AmanSalman <rapamansalman@gmail.com>
2024-05-04 01:28:07 +0300

    "new deplyment"

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ import express from 'express'
 import 'dotenv/config'
 import { connectDB } from './DB/connection.js';
 import { Appinit } from './src/Appinit.js';
-import cors from 'cors'
+import cors from 'cors';
 const app = express();
-const PORT = process.env.PORT || 3000; 
-const allowedOrigins = ["http://localhost".PORT , "https://dashboardgraduation.onrender.com"];
-
+const PORT = process.env.PORT || 3000;
+const allowedOrigins = ["http://localhost:5173", "https://dashboardgraduation.onrender.com"];
 
 app.use(cors({
   origin: (origin, callback) => {
@@ -25,7 +24,7 @@ app.use(cors({
  Appinit(app,express);
 
  connectDB().then(()=>{
-    app.listen(PORT, '0.0.0.0',  ()=>{
+    app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`);
 })
  }).catch(err =>{
