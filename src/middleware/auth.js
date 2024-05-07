f85d0137a478838b9def0ee0e7ff878d0ba3e529
AmanSalman <rapamansalman@gmail.com>
2024-05-08 02:19:40 +0300

    "edit select"

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,13 +7,15 @@ export const auth = ()=>{
         if(!authorization?.startsWith(process.env.BEARERTOKEN)) {
             return res.status(401).json({message:"unauthorized"});
         }
+        console.log(authorization.split(process.env.BEARERTOKEN)[1])
         const token = authorization.split(process.env.BEARERTOKEN)[1];
+
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
         if(!decoded) {
             return res.status(401).json({message:"unauthorized"});
         }
 
-        const user = await UserModel.findById(decoded.id).select({username});
+        const user = await UserModel.findById(decoded.id).select("username");
         if(!user) {
             return res.status(401).json({message:"user not found"});
         }
