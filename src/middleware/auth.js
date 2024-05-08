714789e179aa8b7c4e54a7c184d3238c8c8c949e
AmanSalman <rapamansalman@gmail.com>
2024-05-08 03:25:48 +0300

    "auth chnages"

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,13 +5,13 @@ export const auth = ()=>{
     return async (req,res,next)=>{
         const {authorization} = req.headers;
         if(!authorization?.startsWith(process.env.BEARERTOKEN)) {
-            return res.status(401).json({message:"unauthorized"});
+            return res.status(401).json({message:"unauthorized1"});
         }
         const token = authorization.split(process.env.BEARERTOKEN)[1];
 
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
         if(!decoded) {
-            return res.status(401).json({message:"unauthorized"});
+            return res.status(401).json({message:"unauthorized2"});
         }
 
         const user = await UserModel.findById(decoded.id).select("username");
