a6f7da7906c47d5957ca9e8692beddb46e4b2dc8
AmanSalman <rapamansalman@gmail.com>
2024-05-09 01:31:22 +0300

    " find error auth"

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,11 +4,11 @@ import jwt from 'jsonwebtoken';
 export const auth = ()=>{
     return async (req,res,next)=>{
         const {authorization} = req.headers;
-        if(!authorization?.startsWith(process.env.BEARERTOKEN)) {
-            return res.status(401).json({message:"unauthorized1"});
-        }
-        const token = authorization.split('AmanGRAD__')[1];
-
+        
+        if(!authorization || !authorization.startsWith(process.env.BEARERTOKEN)){
+            return res.status(400).json({message:"invalid authorization"});
+          }
+        const token = authorization.split(process.env.BEARERTOKEN)[1];
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
         if(!decoded) {
             return res.status(401).json({message:"unauthorized2"});
